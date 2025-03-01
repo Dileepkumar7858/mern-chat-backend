4a1e53956e2e8cff98bfd18d14dd09d7da7b6f6f
Dileepkumar7858 <116802870+Dileepkumar7858@users.noreply.github.com>
2025-03-01 15:16:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://graceful-rabanadas-d7913e.netlify.app/"],
     // "https://masync-chat-app.netlify.app"],
     methods: ["GET", "POST"],
     credentials: true,
